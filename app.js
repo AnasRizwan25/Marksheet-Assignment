db4e5cf6f7b66b12dd12ab16307fa49c5b76fc28
AnasRizwan25 <anasgame024@gmail.com>
2024-10-08 15:29:52 +0500

    Assignment Complete

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let marks = [];
 let subject = ['Discrete Structure','Digital Logic Design','Applied Physics','Data Structure','Organizational Behavior'];
 let userName = prompt('Enter The Name'); 
+let userRoll = prompt('Enter The Roll No'); 
 document.getElementById('js-name').innerHTML = userName;
 function percentage(marks){
   let totalMarks = marks.length * 100;
@@ -10,6 +11,17 @@ function percentage(marks){
   });
   let percent = (obtainedMarks * 100) / totalMarks;
 return percent;
+}
+function totalMarks(marks){
+  let totalMarks = marks.length * 100;
+return totalMarks;
+}
+function obtainedMarks(marks){
+  let obtainedMarks = 0;
+  marks.forEach(i => {
+    obtainedMarks +=i;
+  });
+return obtainedMarks;
 }
 for(let i = 0;i < 5;i++){
   let input;
@@ -22,6 +34,8 @@ for(let i = 0;i < 5;i++){
 }
 
 let percent = percentage(marks);
+let obtainedMark = obtainedMarks(marks);
+let totalMark = totalMarks(marks); 
 
 function grading(percent){
   if(percent > 84 && percent <=100) return 'A';
@@ -35,7 +49,7 @@ let grade = grading(percent);
 function scholarS(grade){
   if(grade == 'A') return '75% Scholarship';
   if(grade == 'B') return '55% Scholarship';
-  if(grade == 'C') return '30% Scholarship';
+  if(grade == 'C') return '15% Scholarship';
   return 'No Scholarship';
 }
 let scholarship = scholarS(grade);
@@ -44,8 +58,11 @@ console.log(percent,grade,scholarship);
 let html = 
 `
     <tr>
+      <th class="th1">Roll No</th>
       <th class="th1">Subject</th>
       <th>Marks</th>
+      <th>Obtained Marks</th>
+      <th>Total Marks</th>
       <th>Grade</th>
       <th>Percentage</th>
       <th>Scholarship</th>
@@ -55,14 +72,15 @@ let html =
     subject.forEach((a,i) =>{
       html += `
       <tr>
-      <td class="sub">${a}</td>
+      <td class="a${i+5}">${userRoll}</td>
+      <td>${a}</td>
       <td class="marks">${marks[i]}</td>
+      <td class="a${i}">${obtainedMark}</td>
+      <td class="a${i}">${totalMark}</td>
       <td class="a${i}">${grade}</td>
       <td class="a${i}">${percent}%</td>
       <td class="a${i}">${scholarship}</td>
       </tr>
   `;
 });
-
-
 document.querySelector('.main').innerHTML = html;
